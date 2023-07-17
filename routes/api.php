ac8c219ddc87d9b5c3b26dc5202c130498e4693e
luca betti sorbelli <luca betti sorbelli>
2023-07-17 16:54:07 +0200

    fix function index(technologies)

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/Project',[ApiController::class, 'index']);
+Route::get('/projects',[ApiController::class, 'index']);
